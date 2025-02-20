0c3f48c22a9be3fa794b8e61fa9a753c8e33d3c3
PauloHGrande <pauloav2013@gmail.com>
2025-02-20 13:18:58 -0300

    Service Implementation parte 1

--- server/src/main/java/io/getarrays/server/service/implementation/ServerServiceImpl.java
+++ server/src/main/java/io/getarrays/server/service/implementation/ServerServiceImpl.java
@@ -1,14 +1,36 @@
 package io.getarrays.server.service.implementation;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Collection;
 
-import io.getarrays.server.model.Server;
-import io.getarrays.server.service.ServerService;
+import org.springframework.stereotype.Service;
 
-public class ServerServiceImpl implements ServerService{
+import io.getarrays.server.enumeration.Status;
+import io.getarrays.server.model.Server;
+import io.getarrays.server.repo.ServerRepo;
+import io.getarrays.server.service.ServerService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
+public class ServerServiceImpl implements ServerService {
+	
+	private final ServerRepo serverRepo = null;
 
 	@Override
 	public Server create(Server server) {
+		log.info("Saving new server: {}", server.getName());
+		server.setImageUrl(setServerImageUrl());
+		return serverRepo.save(server);
+	}
+
+	private String setServerImageUrl() {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -38,9 +60,13 @@ public class ServerServiceImpl implements ServerService{
 	}
 
 	@Override
-	public Server ping(String ipAddress) {
-		// TODO Auto-generated method stub
-		return null;
+	public Server ping(String ipAddress) throws IOException {
+		log.info("Pinging server IP: {}", ipAddress);
+		Server server = serverRepo.findByIpAddress(ipAddress);
+		InetAddress address = InetAddress.getByName(ipAddress);
+		server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+		serverRepo.save(server);
+		return server;
 	}
 
 }
