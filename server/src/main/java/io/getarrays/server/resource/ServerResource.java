ebc1c8ae2616308768fbb14e369f3e6d561f7d3e
PauloHGrande <pauloav2013@gmail.com>
2025-02-20 15:07:41 -0300

    API Resource Parte 3

--- server/src/main/java/io/getarrays/server/resource/ServerResource.java
+++ server/src/main/java/io/getarrays/server/resource/ServerResource.java
@@ -1,11 +1,15 @@
 package io.getarrays.server.resource;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
-import java.util.Map;import javax.xml.transform.OutputKeys;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,5 +68,36 @@ public class ServerResource {
 						.statusCode(HttpStatus.CREATED.value())
 						.build()
 				);
+	}
+	
+	@GetMapping("/get/{id}")
+	public ResponseEntity<Response> getServer(@PathVariable("id") Long id) {
+		return ResponseEntity.ok(
+				Response.builder()
+						.timeStamp(LocalDateTime.now())
+						.data(Map.of("servers", serverService.get(id)))
+						.message("Server retrieved")
+						.status(HttpStatus.OK)
+						.statusCode(HttpStatus.OK.value())
+						.build()
+				);
+	}
+	
+	@DeleteMapping("/get/{id}")
+	public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
+		return ResponseEntity.ok(
+				Response.builder()
+						.timeStamp(LocalDateTime.now())
+						.data(Map.of("deleted", serverService.delete(id)))
+						.message("Server deleted")
+						.status(HttpStatus.OK)
+						.statusCode(HttpStatus.OK.value())
+						.build()
+				);
+	}
+	
+	@GetMapping(path =  "/image/{fileName}", produces = MediaType.IMAGE_PNG_VALUE)
+	public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
+		return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "Downloads/images/" + fileName));
 	}
 }
