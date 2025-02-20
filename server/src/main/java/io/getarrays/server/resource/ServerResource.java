4515ad1a9686e56da617c24fa14810622db306c9
PauloHGrande <pauloav2013@gmail.com>
2025-02-20 14:20:22 -0300

    API Resource Parte 2

--- server/src/main/java/io/getarrays/server/resource/ServerResource.java
+++ server/src/main/java/io/getarrays/server/resource/ServerResource.java
@@ -1,16 +1,23 @@
 package io.getarrays.server.resource;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Map;import javax.xml.transform.OutputKeys;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.getarrays.server.enumeration.Status;
 import io.getarrays.server.model.Response;
+import io.getarrays.server.model.Server;
 import io.getarrays.server.service.implementation.ServerServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -29,7 +36,33 @@ public class ServerResource {
 						.status(HttpStatus.OK)
 						.statusCode(HttpStatus.OK.value())
 						.build()
-				)
+				);
 	}
 
+	@GetMapping("/ping/{ipAddress}")
+	public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+		Server server = serverService.ping(ipAddress);
+		return ResponseEntity.ok(
+				Response.builder()
+						.timeStamp(LocalDateTime.now())
+						.data(Map.of("servers", server))
+						.message(server.getStatus() == Status.SERVER_DOWN ? "Ping success" : "Ping failed")
+						.status(HttpStatus.OK)
+						.statusCode(HttpStatus.OK.value())
+						.build()
+				);
+	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<Response> pingServer(@RequestBody @Valid Server server) {
+		return ResponseEntity.ok(
+				Response.builder()
+						.timeStamp(LocalDateTime.now())
+						.data(Map.of("servers", serverService.create(server)))
+						.message("Server created")
+						.status(HttpStatus.CREATED)
+						.statusCode(HttpStatus.CREATED.value())
+						.build()
+				);
+	}
 }
